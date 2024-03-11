81c0e17484f555f72b28d58c980594d86383b6cc
sxporciu <sxporciu@sentara.com>
2024-03-11 14:23:02 -0400

    Fix vuln 'Arbitrary file access during archive extraction ("Zip Slip")'

--- code/src/Attendee/Attendee.cs
+++ code/src/Attendee/Attendee.cs
@@ -10,6 +10,11 @@ namespace Attendees
         public void WriteToDirectory(ZipArchiveEntry entry, string destDirectory)
         {
             string destFileName = Path.Combine(destDirectory, entry.FullName);
+            string fullDestDirPath = Path.GetFullPath(destDirectory + Path.DirectorySeparatorChar);
+            if (!destFileName.StartsWith(fullDestDirPath))
+            {
+                throw new System.InvalidOperationException("Entry is outside the target dir: " + destFileName);
+            }
             entry.ExtractToFile(destFileName);
         }
         
