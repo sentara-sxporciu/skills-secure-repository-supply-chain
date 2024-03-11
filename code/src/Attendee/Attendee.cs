2ff5e4480aa6327a9f6926ecc2454784f95af743
sxporciu <sxporciu@sentara.com>
2024-03-11 15:22:38 -0400

    test sast remediation

--- code/src/Attendee/Attendee.cs
+++ code/src/Attendee/Attendee.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
 
@@ -9,6 +10,7 @@ namespace Attendees
     {
         public void WriteToDirectory(ZipArchiveEntry entry, string destDirectory)
         {
+            /*
             string destFileName = Path.Combine(destDirectory, entry.FullName);
             string fullDestDirPath = Path.GetFullPath(destDirectory + Path.DirectorySeparatorChar);
             if (!destFileName.StartsWith(fullDestDirPath))
@@ -16,6 +18,8 @@ namespace Attendees
                 throw new System.InvalidOperationException("Entry is outside the target dir: " + destFileName);
             }
             entry.ExtractToFile(destFileName);
+            */
+            Debug.Write("test");
         }
         
         public bool AddAttendee(string added)
